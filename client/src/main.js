52d1a986100755cc1c001e335c6e8c7e3c407e3c
Hayouung <hayouung@gmail.com>
2022-05-08 22:50:45 +0100

    fix: no analytics >:(

--- client/src/main.js
+++ client/src/main.js
@@ -3,32 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import SimpleAnalytics from "simple-analytics-vue";
-import VueGtag from "vue-gtag";
 // import Ads from "vue-google-adsense";
 
 Vue.config.productionTip = false;
-
-// analytics
-Vue.use(SimpleAnalytics, {
-  skip: process.env.NODE_ENV !== "production",
-  domain: "api.scuffeduno.online",
-});
-
-Vue.use(
-  VueGtag,
-  {
-    config: {
-      id: "G-GFV5FXX1BW",
-    },
-    // appName: "Scuffed Uno",
-    // pageTrackerScreenviewEnabled: true,
-  },
-  router
-);
-
-// Vue.use(require("vue-script2"));
-// Vue.use(Ads.Adsense);
 
 new Vue({
   router,
