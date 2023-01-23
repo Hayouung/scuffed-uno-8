ac9edb3fa85b42ebc71149d2a77146d5b2913e9f
Hayouung <hayoung@tails.com>
2023-01-23 10:11:34 +0000

    revert: return to scuffed railway

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 const SERVER_URL =
   process.env.NODE_ENV === "development"
     ? `http://${window.location.hostname}:3000`
-    : "167.172.50.164:3000";
+    : "https://scuffed-uno-8.up.railway.app/";
 
 const socket = io(SERVER_URL);
 store.commit("SET_SOCKET", socket);
